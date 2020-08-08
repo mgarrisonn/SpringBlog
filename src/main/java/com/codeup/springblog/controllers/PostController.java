535852d7c40cd33cde947a9d78dcb1c6f521c634
michael garrison <mgarrison.sa@gmail.com>
2020-08-07 21:28:27 -0500

    changed controller method

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -46,37 +46,71 @@ public class PostController {
         return "posts/show";
     }
 
+    // without form model binding
+//    @GetMapping("/posts/create")
+//    public String create() {
+//        return "posts/create";
+//    }
+
+    // without form model binding
+
+//    @PostMapping("/posts/create")
+//    public String insert(@RequestParam String title, @RequestParam String body) {
+//        User user = usersDao.getOne(1L);
+//        Post post = new Post(title, body, user);
+//        postsDao.save(post);
+//        return "redirect:/posts";
+//    }
+
+    // without form model binding
+
+//    @GetMapping("/posts/{id}/edit")
+//    public String editForm(@PathVariable long id, Model model){
+//        model.addAttribute("post", postsDao.getOne(id));
+//        return "posts/edit";
+//    }
+
+    // without form model binding
+//    @PostMapping("/posts/{id}/edit")
+//    public String update(@PathVariable long id, @RequestParam String title, @RequestParam String body){
+//        Post postToEdit = postsDao.getOne(id);
+//        postToEdit.setTitle(title);
+//        postToEdit.setBody(body);
+//        postsDao.save(postToEdit);
+//        return "redirect:/posts/" + id;
+//    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id){
+        postsDao.deleteById(id);
+        return "redirect:/posts";
+    }
+
     @GetMapping("/posts/create")
-    public String create() {
+    public String showPostForm(Model model){
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String insert(@RequestParam String title, @RequestParam String body) {
+    public String createPost(@ModelAttribute Post post){
         User user = usersDao.getOne(1L);
-        Post post = new Post(title, body, user);
+        post.setAuthor(user);
         postsDao.save(post);
         return "redirect:/posts";
     }
 
     @GetMapping("/posts/{id}/edit")
-    public String editForm(@PathVariable long id, Model model){
+    public String showEditForm(@PathVariable Long id, Model model){
         model.addAttribute("post", postsDao.getOne(id));
         return "posts/edit";
     }
 
     @PostMapping("/posts/{id}/edit")
-    public String update(@PathVariable long id, @RequestParam String title, @RequestParam String body){
-        Post postToEdit = postsDao.getOne(id);
-        postToEdit.setTitle(title);
-        postToEdit.setBody(body);
-        postsDao.save(postToEdit);
-        return "redirect:/posts/" + id;
-    }
-
-    @PostMapping("/posts/{id}/delete")
-    public String deletePost(@PathVariable long id){
-        postsDao.deleteById(id);
+    public String editPost(@PathVariable long id, Post post){
+        User user = usersDao.getOne(1L);
+        post.setAuthor(user);
+        postsDao.save(post);
         return "redirect:/posts";
     }
 
