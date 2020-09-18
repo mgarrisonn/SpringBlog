0ea13d8fe22fb1444d910321e613499d55feb46b
michael garrison <mgarrison.sa@gmail.com>
2020-09-17 20:47:17 -0500

    create comment maping-post/create view

--- src/main/java/com/codeup/springblog/controllers/CommentController.java
+++ src/main/java/com/codeup/springblog/controllers/CommentController.java
@@ -1,10 +1,14 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Comment;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.CommentRepository;
+import com.codeup.springblog.repositories.PostRepository;
+import com.codeup.springblog.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,10 +16,14 @@ import java.util.List;
 public class CommentController {
 
     private final CommentRepository commentDao;
+    private final UserRepository userDao;
+    private final PostRepository postDao;
 
 
-    public CommentController(CommentRepository commentDao) {
+    public CommentController(CommentRepository commentDao, UserRepository userDao, PostRepository postDao) {
         this.commentDao = commentDao;
+        this.userDao = userDao;
+        this.postDao = postDao;
     }
 
     @GetMapping("/comments")
@@ -25,6 +33,21 @@ public class CommentController {
         return "comments/index";
     }
 
+    @GetMapping("/comments/create/{id")
+    public String showCreateCommentForm(Model model, @PathVariable Long id) {
+        User user = userDao.findByUsername(postDao.getOne(id));
+        model.addAttribute("user", user);
+        model.addAttribute("comment", new Comment());
+        return "comments/create";
+    }
 
+    @PostMapping("comments/create")
+    public String createComment(@ModelAttribute Comment comment, Model model, @RequestParam(name = "userId") Long userId, Long id) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userC = userDao.findByUsername(userDao.getOne(userId));
+        comment.setContent(userC.getUsername());
+        comment.setParentPost(user.getId(id));
+        return "redirect:/posts";
+    }
 
 }
